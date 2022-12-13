a7ba930d243476516e6d0092a4211f0bb9a390e2
Dop <dopddaiga@gmail.com>
2022-12-13 16:31:52 +0100

    add Category and Product views

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
-# Create your views here.
+# Building catalog views
+# Product list  views
+def product_list(request, category_slug=None):
+    category_slug = None
+    categories = Category.objects.all()
+    # retrieve only available products.
+    products = Product.objects.filter(available=True)
+    # optionally filter products by a given category
+    if category_slug:
+        category = get_object_or_404(Category,
+                                    slug=category_slug)
+        products = products.filter(category=category)
+
+    return render(request, 'shop/product/list.html',
+                    {'category': category,
+                    'categories': categories,
+                    'products': products})
+
+# Product detail views
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id,
+                                slug=slug, available=True)
+
+# retieves the product instance based on id and slug params
+    return render(request,
+                    'shop/product/detail.html',
+                    {'product': product})
